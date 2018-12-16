f4e8838da07cd175230feea102c3ca7c8cd44e69
Sven Dowideit <SvenDowideit@home.org.au>
2018-12-16 13:22:20 +1000

    slightly safer relay, and lets get the port right
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- Makefile
+++ Makefile
@@ -2,5 +2,8 @@
 mqtt:
 	mosquitto_sub -h mqtt.thegame.folly.site -v -t +/+/+ -u everyone -P S4C7Tzjc2gD92y9
 
-post:
-	mosquitto_pub -h mqtt.thegame.folly.site -u everyone -P S4C7Tzjc2gD92y9 -t asdf/asdf/asdf -m '{}'
+site:
+	mosquitto_pub -h mqtt.thegame.folly.site -u everyone -P S4C7Tzjc2gD92y9 -t asdf/asdf/asdf -m '{"to": "folly.site"}'
+
+local:
+	mosquitto_pub -h mqtt.local -t asdf/asdf/asdf -m '{"to": "folly.site"}'
--- src/RPi/mqtt/main.py
+++ src/RPi/mqtt/main.py
@@ -24,8 +24,8 @@ hostmqtt = mqtt.MQTT(mqttHost, myHostname, "relay_from")
 hostmqtt.loop_start()   # use the background thread
 
 master_mqtt_host = config.getValue("mqttmaster", "mqtt.thegame.folly.site")
-mastermqtt = mqtt.MQTT(master_mqtt_host, myHostname, "relay_to", "everyone", "S4C7Tzjc2gD92y9", 8883)
-mastermqtt.loop_start()   # use the background thread
+mastermqtt = mqtt.MQTT(master_mqtt_host, myHostname, "relay_to", "everyone", "S4C7Tzjc2gD92y9", 1883)
+#mastermqtt.loop_start()   # use the background thread
 
 # end load config
 
@@ -37,43 +37,52 @@ def muteAll():
 # subscription handlers
 
 def relay_message_to_master(topic, payload):
-    # Only relay messages from our local mqtt to the global one if they havn't already been relayed
-    if "relay_from" in payload:
-        #print("relayed before "+payload["relay_from"])
-        return
+    try:
+        # Only relay messages from our local mqtt to the global one if they havn't already been relayed
+        if "relay_from" in payload:
+            #print("relayed before "+payload["relay_from"])
+            return
 
-    payload["relay_from"] = myHostname
-    print("Relaying to master: %s, %s" % (topic, payload))
-    mastermqtt.relay(topic, payload)
+        payload["relay_from"] = myHostname
+        print("Relaying to master: %s, %s" % (topic, payload))
+        mastermqtt.relay(topic, payload)
+    except Exception as ex:
+        traceback.print_exc()
 
 
 def relay_message_from_master(topic, payload):
-    # Only relay messages from our local mqtt to the global one if they havn't already been relayed
-    if not "relay_from" in payload:
-        payload["relay_from"] = master_mqtt_host
-    else:
-        if payload["relay_from"] == myHostname:
-            # don't relay a message that originated with us...
-            return
+    try:
+        # Only relay messages from our local mqtt to the global one if they havn't already been relayed
+        if not "relay_from" in payload:
+            payload["relay_from"] = master_mqtt_host
+        else:
+            if payload["relay_from"] == myHostname:
+                # don't relay a message that originated with us...
+                return
 
-    print("Relaying from master: %s, %s" % (topic, payload))
-    hostmqtt.relay(topic, payload)
+        print("Relaying from master: %s, %s" % (topic, payload))
+        hostmqtt.relay(topic, payload)
+    except Exception as ex:
+        traceback.print_exc()
 
 ########################################
 
 
 hostmqtt.subscribeL("+", "+", "+", relay_message_to_master)
 print(hostmqtt.sub)
-mastermqtt.subscribeL("+", "+", "+", relay_message_from_master)
+#mastermqtt.subscribeL("+", "+", "+", relay_message_from_master)
 
 hostmqtt.status({"status": "listening"})
+mastermqtt.status({"status": "listening"})
 
 try:
-    while True:
-        sleep(1)
+    #while True:
+    #    sleep(1)
+    mastermqtt.loop_forever()
 except Exception as ex:
     traceback.print_exc()
 except KeyboardInterrupt:
     print("exit")
 
 hostmqtt.status({"status": "STOPPED"})
+mastermqtt.status({"status": "STOPPED"})
