5f6f3d0d3fd8ebc277830e5838814f247d237113
Sven Dowideit <SvenDowideit@home.org.au>
2018-12-23 06:11:17 +0000

    In 1819/src/RPi/buzzswitch added test4th1.py This can successfully display state and  state changes of buzzwire switch by printing to console. This needs to be converted into signals to the main server.

---
+++ src/RPi/buzzswitch/test1.py
@@ -0,0 +1,69 @@
+
+
+import time
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BOARD)
+
+GPIO.setup(15,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(13,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(11,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+state = -1
+
+aTouched = False
+bTouched = False
+cTouched = False
+
+#GPIO.setup(7,GPIO.OUT)
+#GPIO.output(7,0)
+
+
+try:
+	while True:
+
+#		GPIO.output(7, GPIO.input(11) )
+		time.sleep(0.02)			
+		if (GPIO.input(11) == 1):
+		
+			print "A"
+
+			if not bTouched:
+				if cTouched:
+					state = 0
+			
+			aTouched = True	
+			bTouched = False
+			cTouched = False
+			
+			print state
+			
+		elif (GPIO.input(13) == 1):
+
+			bTouched = True
+
+			print "B"
+			print state
+			
+		elif (GPIO.input (15) == 1):
+
+			print "C"
+			
+			if not bTouched:
+				if aTouched:
+					state = 1
+
+			aTouched = False
+			bTouched = False
+			cTouched = True
+			print state
+			
+			
+		else:
+#			GPIO.output(7,0)
+			print "NO TOUCHING"
+
+			print state
+
+except KeyboardInterrupt:
+	GPIO.cleanup()
