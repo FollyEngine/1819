cd1418fed63d41d6aa00e1a623431d3f5e42eae0
Huon Kane <huon.kane@hotmail.co.uk>
2018-12-23 18:38:09 +1000

    Updated Lever switch program in 1819/src/RPi/switch test3rd1 is current version Can display  state and state change via print,  however cannot broadcast on network

--- src/RPi/switch/test1.py
+++ src/RPi/switch/test1.py
@@ -1,15 +1,32 @@
 import RPi.GPIO as GPIO
+import time
 
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(11,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(22,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.setup(7,GPIO.OUT)
-GPIO.output(7,0)
+state = -1
+#GPIO.setup(7,GPIO.OUT)
+#GPIO.setup(40,GPIO.OUT)
+#GPIO.output(7,0)
+#GPIO.output(40,0)
 
 try:
 	while True:
-		GPIO.output(7, GPIO.input(11) )
+
+		time.sleep(0.3)
+		if (GPIO.input(11) == 1):
+			if (state != 0):
+				print "STATE CHANGE"
+			state = 0
+			print state
+
+		if (GPIO.input(22) == 1):
+			if (state != 1):
+				print "STATE CHANGE"
+			state = 1
+			print state
 
 except KeyboardInterrupt:
 	GPIO.cleanup()
