75fab7e10312906c69a37c4c42a325f99de00692
andrewlorien <andrew.lorien@gruden.com>
2018-12-24 18:40:48 +1000

    check for git updates and restart main script

--- update.sh
+++ update.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+# Update repository, and if our main file has changed, restart it
+OLD_HEAD=$(git rev-parse HEAD)
 git pull
-python ./src/RPi/$HOSTNAME/main.py
+NEW_HEAD=$(git rev-parse HEAD)
+git diff --name-only $OLD_HEAD $NEW_HEAD ./src/RPi/$HOSTNAME/main.py | grep main.py
+if [ ! $? ] ; then
+  # our main file was updated.  kill the running one and start a new one
+    kill $(cat main_pid)
+    nohup python ./src/RPi/$HOSTNAME/main.py &
+    echo $! > main_pid
+fi
+
 ##  this "healthy" report just says that we're turned on and online.  
 ## TODO : check whether the python script is actually running without error
 ## TODO : report when an update has occurred
