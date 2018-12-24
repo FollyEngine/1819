b2f3ffb494228373cf1b35c8552e56557c2c0e5e
andrewlorien <andrew.lorien@gruden.com>
2018-12-24 17:47:56 +1000

    publish health to mqtt

--- update.sh
+++ update.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 git pull
-python ./src/RPi/$HOSTNAME/main.py >> local.log
+python ./src/RPi/$HOSTNAME/main.py
+##  this "healthy" report just says that we're turned on and online.  
+## TODO : check whether the python script is actually running without error
+## TODO : report when an update has occurred
+mosquitto_pub -h "mqtt.thegame.folly.site"  -u mqtt.thegame.folly.site -P S4C7Tzjc2gD92y9  -t "$HOSTNAME/$HOSTNAME/status" -m '{"status":"healthy"}'
