ab4be50bacfd55942562fdb06360c013494f6a58
andrewlorien <andrew.lorien@gruden.com>
2018-12-25 12:04:46 +1000

    update health status

--- src/RPi/secretswitch/readme.md
+++ src/RPi/secretswitch/readme.md
@@ -15,7 +15,9 @@ TODO:
 * Continuous Deployment + health ping
 .* make a bash script that does mqtt health status ; git pull ; restart buzzwire monitor if new
 .* crontab like this
-  0/5 * * * * cd 1819 && update.sh > local.log 2>&1
+  @reboot cd ~/1819 ; rm main_pid
+  5,10,15,20,25,30,35,40,45,50,55 * * * * cd ~/1819 && ./update.sh >> local.log 2>&1
+
 
 
 NOTE : whichever dongle has SSID TPU4G_T39Y has psk 63619637
--- update.sh
+++ update.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# If the main process (main.py) belonging to this host is not running, start it.
+# If that script has been updated in github, restart it.
+# To make this work, add these two lines to your crontab (crontab -e)
+# @reboot cd ~/1819 ; rm main_pid
+# 5,10,15,20,25,30,35,40,45,50,55 * * * * cd ~/1819 && ./update.sh >> local.log 2>&1
+
+
 ## This script requires that main_pid does not exist on system start.
 if [ ! -f main_pid ] ; then
    nohup python ./src/RPi/$HOSTNAME/main.py &
@@ -19,4 +26,4 @@ fi
 ##  this "healthy" report just says that we're turned on and online.  
 ## TODO : check whether the python script is actually running without error
 ## TODO : report when an update has occurred
-mosquitto_pub -h "mqtt.thegame.folly.site"  -u mqtt.thegame.folly.site -P S4C7Tzjc2gD92y9  -t "$HOSTNAME/$HOSTNAME/status" -m '{"status":"healthy"}'
+mosquitto_pub -h "mqtt.thegame.folly.site"  -u mqtt.thegame.folly.site -P S4C7Tzjc2gD92y9  -t "$HOSTNAME/$HOSTNAME/health" -m "{\"status\":\"healthy\",\"time\":\"$(date +%Y-%m-%dT%H:%M:%S)\",\"device\":\"$HOSTNAME\"}"
