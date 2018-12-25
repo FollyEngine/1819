85a2e9b3c5574f592f8d84f03a81aeb6085afbf1
andrewlorien <andrew.lorien@gruden.com>
2018-12-25 15:26:24 +1000

    rasberry pis are all UTC not local time

--- update.sh
+++ update.sh
@@ -26,4 +26,4 @@ fi
 ##  this "healthy" report just says that we're turned on and online.  
 ## TODO : check whether the python script is actually running without error
 ## TODO : report when an update has occurred
-mosquitto_pub -h "mqtt.thegame.folly.site"  -u mqtt.thegame.folly.site -P S4C7Tzjc2gD92y9  -t "$HOSTNAME/$HOSTNAME/health" -m "{\"status\":\"healthy\",\"time\":\"$(date +%Y-%m-%dT%H:%M:%S)\",\"device\":\"$HOSTNAME\"}"
+mosquitto_pub -h "mqtt.thegame.folly.site"  -u mqtt.thegame.folly.site -P S4C7Tzjc2gD92y9  -t "$HOSTNAME/$HOSTNAME/health" -m "{\"status\":\"healthy\",\"time\":\"$(date +%Y-%m-%dZ%H:%M:%S)\",\"device\":\"$HOSTNAME\"}"
