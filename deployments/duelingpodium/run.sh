a0b2543fd0389f7179989b0222b09d645f00136b
andrewlorien <andrew.lorien@gruden.com>
2018-12-26 11:55:12 +1000

    git pull isn't reliable until network is up

--- deployments/duelingpodium/run.sh
+++ deployments/duelingpodium/run.sh
@@ -9,14 +9,12 @@ CONFIGFILE=
 
 # Auto start by adding the following to the RPi pi user crontab
 # @reboot cd 1819; ./deployments/duelingpodium/run.sh > duelingpodium.log 2>&1
-# TODO : update this script to match the 1819/update.sh script which does a health check every 5min and restarts main.py on git update
+
 
 echo "$WHOAMI in $(pwd)"
 echo "talking to message queue server: $MQTTHOST"
 echo "packages: $PACKAGES"
 
-sleep 1  # wait a moment for network to come up
-git pull
 if [[ ¨$1¨ == ¨--setup¨ ]]; then
 	echo "Running Setup"
 	# get pyscard
@@ -25,6 +23,7 @@ if [[ ¨$1¨ == ¨--setup¨ ]]; then
 	sudo apt-get install -yq python3-pyscard python3-pip pcsc-tools pcscd git python3-setuptools libpcsclite-dev python3-dev \
 				mosquitto-clients mosquitto scratch python-pygame
 
+	git pull
 
 	for pkg in $PACKAGES; do
 		if [[ -f "./src/RPi/$pkg/requirements.txt" ]]; then
