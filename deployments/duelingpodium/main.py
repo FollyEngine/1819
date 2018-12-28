fd0e3bac7768edb7ca958451aec5b22ed5b0c598
Sven Dowideit <SvenDowideit@home.org.au>
2018-12-29 06:36:11 +1000

    wrong boost
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- deployments/duelingpodium/main.py
+++ deployments/duelingpodium/main.py
@@ -187,9 +187,9 @@ def reconcile_magic():
                 print('i boosted')
                 #boost attack and counter for next round (or again and again) - again, use the round number
                 print("boosting Attack from: %d" % playerCurrentState['Attack'])
-                playerCurrentState['Attack'] = playerCurrentState['Attack'] + playerCurrentState['Attack'] * (playerCurrentState['Boost']/100)
-                print("boosting Attack to: %d" % playerCurrentState['Attack'])
-                playerCurrentState['Counter'] = playerCurrentState['Attack'] + playerCurrentState['Counter'] * (playerCurrentState['Boost']/100)
+                playerCurrentState['Attack'] = playerCurrentState['Attack'] + (playerCurrentState['Attack'] * (playerCurrentState['Boost']/100))
+                print("boosting Counter to: %d" % playerCurrentState['Counter'])
+                playerCurrentState['Counter'] = playerCurrentState['Counter'] + (playerCurrentState['Counter'] * (playerCurrentState['Boost']/100))
                 skip_ABC_reset = 1
         if their_magic_cast['modifier'] == 'disable':
             print('they cast disable')
