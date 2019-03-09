892385954ab100b3bb587041012c4d6df624dc3d
Sven Dowideit <SvenDowideit@home.org.au>
2019-03-09 12:26:42 +1000

    use the devicename for logfile name
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- src/RPi/mqtt/config.py
+++ src/RPi/mqtt/config.py
@@ -35,7 +35,9 @@ def getDevicename():
 configFile = "config.yml"
 cfg = {}
 
-logFile = getDeploymentType()+"-"+getDevicename()+".log"
+logFile = sys.argv[0].replace("/", "-").replace(".py", ".log").replace(".-", "")
+if len(sys.argv) != 4:
+    logFile = getDeploymentType()+"-"+getDevicename()+".log"
 logging.basicConfig(
      level=getValue("loglevel", logging.DEBUG),
  )
