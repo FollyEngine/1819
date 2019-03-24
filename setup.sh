465e29b6e7901b398a56b95dfbb6e8d3cbb79503
Sven Dowideit <SvenDowideit@home.org.au>
2019-03-24 14:46:20 +1000

    we're going to try comitup
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ sudo apt-get upgrade -yq
 sudo apt-get install -yq python3-pyscard python3-pip pcsc-tools pcscd git python3-setuptools libpcsclite-dev python3-dev \
 			mosquitto-clients mosquitto scratch python-pygame \
 			python3-serial python-serial python-pip python-pyscard \
-			vim
+			vim comitup
 
 git pull
 
