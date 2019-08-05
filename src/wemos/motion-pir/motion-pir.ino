3feff8048d9e56817a8c30e6d83670040eec2f27
Sven Dowideit <SvenDowideit@home.org.au>
2019-08-05 20:56:19 +1000

    pir works, but its pretty slow
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

---
+++ src/wemos/motion-pir/motion-pir.ino
@@ -0,0 +1,48 @@
+
+
+// secrets.h defines SECRET_SSID and SECRET_PASSWORD (and we assume the local network has an accessible mqtt host)
+#include "secrets.h"
+
+#include "mqtt.h"
+
+#include <ArduinoJson.h>
+
+char deviceType[] = "motion";
+
+//BUILD with "LOLIN(WEMOS) D1 R2 & mini"
+Mqtt mqtt = Mqtt(SECRET_SSID, SECRET_PASSWORD, "mqtt.local", 1883, deviceType);
+
+void mqtt_callback_fn(const char* topic, byte* payload, unsigned int length) {
+  Serial.printf("Callback: %s\n", topic);
+}
+
+void setup() {
+  //while (!Serial);
+  Serial.begin(115200);
+  Serial.printf("Started\n");
+
+  mqtt.setCallback(mqtt_callback_fn);
+  mqtt.setup();
+}
+
+int motionState = LOW;
+unsigned long nextStatusMessage = 0;
+void loop() {
+  mqtt.loop();  // Note - this sends a regular status:listening message - every 5minutes
+
+  // PIR sensor is on pin D3
+  int val = digitalRead(D3);
+  //low = no motion, high = motion
+  if (val != motionState) {
+    motionState = val;
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+    if (motionState == LOW) {
+      root["pir"] = "no motion";
+    } else {
+      root["pir"] = "motion detected";
+    }
+    mqtt.publish(deviceType, "motion", root);
+    
+  }
+}
